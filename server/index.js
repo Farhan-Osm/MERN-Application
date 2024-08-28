715d38891a750ca59b3123dbf433d29683efe27c
Farhan <farhanosm121@gmail.com>
2024-08-28 20:00:53 +0530

    build folder added

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-// app.use(express.json())
+app.use(express.static("dist"))
 
 app.get("/", (req, res) => {
   res.send("Hello how are you");
